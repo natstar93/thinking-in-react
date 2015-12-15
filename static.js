29ba332681f79e442a62da8524c698421346481e
Natalie Akam <natalie.akam@gmail.com>
2015-12-15 22:52:34 +0000

    Added SearchBar component

--- static.js
+++ static.js
@@ -3,7 +3,17 @@ import React from 'react'
 export const FilterableProductTable = React.createClass({
   render() {
     return (
-      <div></div>
+      <div><SearchBar/></div>
     );
   }
+});
+
+export const SearchBar = React.createClass({
+  render: function() {
+        return (
+          <div>
+            <input type="search" placeholder="Search..." />
+          </div>
+        );
+    }
 });
